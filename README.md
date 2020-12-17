07b7b104995a00da4bafb7c082d86bbdb1a6fdbb
Robinnsson Cifuentes Ramírez <robinnsson@gmail.com>
2020-12-16 23:00:33 -0500

    Muestra colores aleatorios cada vez que se da click

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # App Color My View: Google Codelab's
 
 Esta es una app que hace parte de una serie de apps realizadas para entrenar el desarrollo de apps nativas Android, las cuales son desarrolladas en lenguaje Kotlin y de acuerdo con los instructivos de Google Developers
+___
+## Modificación propia
 
-## Codelab 2.3
+He modificado el código para que al clickear cada elemento el color de relleno se establece aleatoriamente, con eso, se podrá cambiar el color del fondo cada vez que se toque el elemento.
+
+Para ello, le paso por parámetro la función `randomColor()` al método `.setBackgroundColor()` de la función `makeColored()`  
+___
 
--- app/src/main/java/com/c2r/colormyviews/MainActivity.kt
+++ app/src/main/java/com/c2r/colormyviews/MainActivity.kt
@@ -19,12 +19,12 @@ class MainActivity : AppCompatActivity() {
         when (view.id) {
             // English: Boxes using Color class colors for the background.
             // Spanish: Las cajas usan los colores de la clase Color para el fondo.
-            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
-            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
-            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
-            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
-            else -> view.setBackgroundColor(Color.LTGRAY)
+            R.id.box_one_text -> view.setBackgroundColor(randomColor())
+            R.id.box_two_text -> view.setBackgroundColor(randomColor())
+            R.id.box_three_text -> view.setBackgroundColor(randomColor())
+            R.id.box_four_text -> view.setBackgroundColor(randomColor())
+            R.id.box_five_text -> view.setBackgroundColor(randomColor())
+            else -> view.setBackgroundColor(randomColor())
         }
     }
 
@@ -43,5 +43,24 @@ class MainActivity : AppCompatActivity() {
         for (click in clickableViews) {
             click.setOnClickListener { makeColored(it) }
         }
+    }
+
+    private fun randomColor(): Int {
+        val item = (1..10).random()
+
+        val color = when (item) {
+            1 -> Color.DKGRAY
+            2 -> Color.GRAY
+            3 -> Color.BLUE
+            4 -> Color.MAGENTA
+            5 -> Color.BLUE
+            6 -> Color.BLACK
+            7 -> Color.GREEN
+            8 -> Color.RED
+            9 -> Color.YELLOW
+            10 -> Color.GRAY
+            else -> Color.WHITE
+        }
+        return color
     }
 }
